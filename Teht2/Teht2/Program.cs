43f7b889c8ae999fce167adb7efdfb9071af5280
reapercage <h4026@student.jamk.fi>
2017-02-01 15:37:35 +0200

    Teht2 valmis, mites foreach?

---
+++ Teht2/Teht2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht2
+{
+    class Tuote
+    {
+        public string nimi;
+        public double hinta;
+        public string Nimi { get; set; }
+        public Tuote()
+        {
+        }
+        public Tuote(string Nimi, double Hinta)
+        {
+            Nimi = nimi;
+            Hinta = hinta;
+        }
+        //ToString...jotain
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Tuote> ostokset = new List<Tuote>();
+            ostokset.Add(new Tuote { nimi = "leikkele", hinta = 1.20 });
+            ostokset.Add(new Tuote { nimi = "voi", hinta = 0.80 });
+            int ostosLukumaara = ostokset.Count();
+            Console.WriteLine("Ostoskärryn sisältö:");
+            //foreach ratkaisukin on?
+            for(int i = 0; i < ostosLukumaara; i++)
+            {
+                Console.WriteLine(ostokset[i].nimi + " " + ostokset[i].hinta + "e");
+            }
+            Console.ReadKey();
+        }
+    }
+}
