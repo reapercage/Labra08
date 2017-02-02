0f31f2289dd9641099cf916242d20fa6b0349702
reapercage <h4026@student.jamk.fi>
2017-02-02 21:30:18 +0200

    Teht4 alku

---
+++ Teht4/Teht4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht4
+{
+    abstract class Shape
+    {
+        public string Name { get; set; }
+        public double Area { get; set; }
+        public double Circumference { get; set; }
+    }
+    class Circle : Shape
+    {
+
+    }
+    class Rectangle : Shape
+    {
+
+    }
+    class Shapes
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
