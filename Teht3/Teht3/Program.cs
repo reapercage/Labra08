fd66c89f792f72a03fb744e1c164956e2765468c
reapercage <h4026@student.jamk.fi>
2017-02-02 13:40:07 +0200

    Teht3 jatkoa

--- Teht3/Teht3/Program.cs
+++ Teht3/Teht3/Program.cs
@@ -8,24 +8,39 @@ namespace Teht3
 {
     class Kala
     {
+        public string kalastaja_id;
+        public string kalapaikka_id;
         public string laji;
         public double pituus;
         public double paino;
     }
     class Kalastaja
     {
+        public string kalastaja_id;
         public string nimi;
         public string puhnro;
     }
     class Kalapaikka
     {
+        public string kalapaikka_id;
         public string nimi;
-        public string paikka;
+        public string sijainti;
     }
     class Program
     {
         static void Main(string[] args)
         {
+            List<Kalastaja> kalastajat = new List<Kalastaja>();
+            kalastajat.Add(new Kalastaja { kalastaja_id = "01", nimi = "Tuhka Ketsuppi", puhnro = "04516447894" });
+
+            List<Kalapaikka> kalapaikat = new List<Kalapaikka>();
+            kalapaikat.Add(new Kalapaikka { kalapaikka_id = "01", nimi = "Satama", sijainti = "Jyväskylä"});
+
+            List<Kala> kalat = new List<Kala>();
+            kalat.Add(new Kala { kalastaja_id = "01", kalapaikka_id = "01", laji = "kuha", pituus = 25.72, paino = 0.578 });
+            kalat.Add(new Kala { kalastaja_id = "01", kalapaikka_id = "01", laji = "ahven", pituus = 12.20, paino = 0.241 });
+
+
         }
     }
 }
