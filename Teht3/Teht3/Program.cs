ae30cb805366a49880f5f6c1a96912242938e4ba
reapercage <h4026@student.jamk.fi>
2017-02-02 16:52:15 +0200

    Teht3 melko pitkällä

--- Teht3/Teht3/Program.cs
+++ Teht3/Teht3/Program.cs
@@ -8,22 +8,23 @@ namespace Teht3
 {
     class Kala
     {
-        public string kalastaja_id;
-        public string kalapaikka_id;
+        public int kala_id;
+        public int kalastaja_id;
+        public int kalapaikka_id;
         public string laji;
         public double pituus;
         public double paino;
     }
     class Kalastaja
     {
-        public string kalastaja_id;
+        public int kalastaja_id;
         public string nimi;
         public string puhnro;
     }
     class Kalapaikka
     {
-        public string kalapaikka_id;
-        public string nimi;
+        public int kalapaikka_id;
+        public string kpaikka;
         public string sijainti;
     }
     class Program
@@ -31,16 +32,69 @@ namespace Teht3
         static void Main(string[] args)
         {
             List<Kalastaja> kalastajat = new List<Kalastaja>();
-            kalastajat.Add(new Kalastaja { kalastaja_id = "01", nimi = "Tuhka Ketsuppi", puhnro = "04516447894" });
+            kalastajat.Add(new Kalastaja { kalastaja_id = 01, nimi = "Tuhka Ketsuppi", puhnro = "04516447894" });
+            for (int i = kalastajat.Count()-1; i < kalastajat.Count(); i++)
+            {
+                Console.WriteLine("Lisätty kalastaja : " + kalastajat[i].kalastaja_id + " " + kalastajat[i].nimi + " " + kalastajat[i].puhnro);
+            }
 
             List<Kalapaikka> kalapaikat = new List<Kalapaikka>();
-            kalapaikat.Add(new Kalapaikka { kalapaikka_id = "01", nimi = "Satama", sijainti = "Jyväskylä"});
+            kalapaikat.Add(new Kalapaikka { kalapaikka_id = 01, kpaikka = "Satama", sijainti = "Jyväskylä"});
+            for(int i = kalapaikat.Count()-1; i < kalapaikat.Count(); i++)
+            {
+                Console.WriteLine("Lisätty kalapaikka : " + kalapaikat[i].kalapaikka_id + " " + kalapaikat[i].kpaikka + " " + kalapaikat[i].sijainti);
+            }
+            //tyhjärivi
+            Console.WriteLine();
 
             List<Kala> kalat = new List<Kala>();
-            kalat.Add(new Kala { kalastaja_id = "01", kalapaikka_id = "01", laji = "kuha", pituus = 25.72, paino = 0.578 });
-            kalat.Add(new Kala { kalastaja_id = "01", kalapaikka_id = "01", laji = "ahven", pituus = 12.20, paino = 0.241 });
+
+            kalat.Add(new Kala { kala_id = 01, kalastaja_id = 01, kalapaikka_id = 01, laji = "kuha", pituus = 25.72, paino = 0.578 });
+            //x etsitään viimeisin kala x muuttujaam kalan tietoihin pääsemiseksi kalastajaa etsiessä
+            int x = 3;
+            for (int i = kalat.Count() - 1; i < kalat.Count(); i++)
+            {
+                x = i;
+            }
+            string kalastajaNimi = "nimi";
+            for (int i = kalastajat.Count() - 1; i < kalastajat.Count(); i++)
+            {
+                if(kalastajat[i].kalastaja_id == kalat[x].kalastaja_id)
+                {
+                    //tallenna kalastajan nimi muuttujaan
+                    kalastajaNimi = kalastajat[i].nimi;
+                }
+            }
+            for (int i = kalat.Count()-1; i < kalat.Count(); i ++)
+            {
+                Console.WriteLine("Kalastaja " + kalastajaNimi + " sai kalan:\n -" + kalat[i].laji + " " + kalat[i].pituus + "cm " + kalat[i].paino + "kg");
+            }
 
 
+            kalat.Add(new Kala { kala_id = 02, kalastaja_id = 01, kalapaikka_id = 01, laji = "ahven", pituus = 12.20, paino = 0.241 });
+            //x kalan tietoihin pääsemiseksi kalastajaa etsiessä
+            //int x = 3;
+            for (int i = kalat.Count() - 1; i < kalat.Count(); i++)
+            {
+                x = i;
+            }
+            //string kalastajaNimi = "nimi";
+            for (int i = kalastajat.Count() - 1; i < kalastajat.Count(); i++)
+            {
+                if (kalastajat[i].kalastaja_id == kalat[x].kalastaja_id)
+                {
+                    //tallenna kalastajan nimi muuttujaan
+                    kalastajaNimi = kalastajat[i].nimi;
+                }
+            }
+            for (int i = kalat.Count() - 1; i < kalat.Count(); i++)
+            {
+                Console.WriteLine("Kalastaja " + kalastajaNimi + " sai kalan:\n -" + kalat[i].laji + " " + kalat[i].pituus + "cm " + kalat[i].paino + "kg");
+            }
+
+            //käy läpi for kalastajat
+            //if  kalastajat[].kalastaja_id == kala[].kalastaja_id , print kala[x].*
+            Console.ReadKey();
         }
     }
 }
