ef064ea0001fc0c52b57c74c592eb7aa66f16145
reapercage <h4026@student.jamk.fi>
2017-02-03 18:34:54 +0200

    Teht6 alkua

---
+++ Teht6/Teht6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht6
+{
+    class Invoice
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        private double total;
+        public double Total
+        {
+            get
+            {
+                return total;
+            }
+            set
+            {
+                total = Price * Quantity;
+            }
+        }
+        //ToString()
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
