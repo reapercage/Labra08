86bb8fe01ee877da78cd34486854f0a0a088dcee
reapercage <h4026@student.jamk.fi>
2017-02-03 12:55:31 +0200

    Teht4 etenee

--- Teht4/Teht4/Program.cs
+++ Teht4/Teht4/Program.cs
@@ -9,19 +9,63 @@ namespace Teht4
     abstract class Shape
     {
         public string Name { get; set; }
-        public double Area { get; set; }
-        public double Circumference { get; set; }
+
+        public abstract void Area();
+
+        public abstract void Circumference();
     }
     class Circle : Shape
     {
+        private double radius;
+        private double area;
+        private double circumference;
+
+        public double Radius
+        {
+            get
+            {
+                return radius;
+            }
+            set
+            {
+                radius = value;
+            }
+        }
+
+        public override void Area()
+        {
+            area = Math.PI * Math.Pow(radius,2);
+        }
+        public override void Circumference()
+        {
+            circumference = 2 * Math.PI * radius;
+        }
 
     }
     class Rectangle : Shape
     {
+        private double width;
+        private double height;
+        private double area;
+        private double circumference;
+
+        public override void Area()
+        {
+            area = width * height;
+        }
+        public override void Circumference()
+        {
+            circumference = 2 * width + 2 * height;
+        }
 
     }
     class Shapes
     {
+        private List<Shape> shapes;
+        public List<Shape> Kuviot
+        {
+            get { return shapes; }
+        }
 
     }
     class Program
