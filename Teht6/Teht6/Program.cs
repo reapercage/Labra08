98fba16b48e9d2021708148238f3cbfa6f04e89d
reapercage <h4026@student.jamk.fi>
2017-02-04 11:47:13 +0200

    Teht6 jotain toimintaa

--- Teht6/Teht6/Program.cs
+++ Teht6/Teht6/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Teht6
 {
-    class Invoice
+    class InvoiceItem
     {
         public string Name { get; set; }
         public double Price { get; set; }
@@ -23,12 +23,51 @@ namespace Teht6
                 total = Price * Quantity;
             }
         }
+        public override string ToString()
+        {
+            return Name + " " + Price + "e " + Quantity + "kpl " + total + "total";
+        }
         //ToString()
+    }
+    class Invoice
+    {
+        private double total;
+        public string Customer { get; set; }
+        private List<InvoiceItem> invoice;
+        public List<InvoiceItem> Ostokset
+        {
+            get { return invoice; }
+        }
+        public Invoice()
+        {
+            invoice = new List<InvoiceItem>();
+        }
+        public void AddItem(InvoiceItem ini)
+        {
+            invoice.Add(ini);
+        }
+        public double Total
+        {
+            get { return Total; }
+            set
+            {
+                for(int i = 0; i < invoice.Count(); i++)
+                {
+                    total = total + invoice[i].Total;
+                }
+            }
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
+            Invoice ostoskori = new Invoice();
+            ostoskori.Customer = "Pertti Keinonen";
+            InvoiceItem ostos = new InvoiceItem { Name = "Eggs", Price = 1.40, Quantity = 1 };
+            ostoskori.AddItem(ostos);
+            Console.WriteLine(ostos.ToString());
+            Console.ReadKey();
         }
     }
 }
